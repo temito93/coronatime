6d59aa5f11f9f5e01854aa96b3d8d3bd1fc881d4
temito93 <temotemo161@gmail.com>
2022-11-24 02:05:15 +0400

    feat: add migrations and model for users_verify

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,18 +5,13 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
 	public function up()
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
 			$table->string('username')->unique();
 			$table->string('email')->unique();
-            $table->boolean('is_email_verified')->default(0);
+			$table->boolean('is_email_verified')->default(0);
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
@@ -24,11 +19,6 @@ return new class extends Migration {
 		});
 	}
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
 	public function down()
 	{
 		Schema::dropIfExists('users');
