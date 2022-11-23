ef75a9c468f0714e6fcf220ce395f72488128fa4
temito93 <temotemo161@gmail.com>
2022-11-24 01:48:55 +0400

    feat: add username in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ return new class extends Migration {
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('name');
+			$table->string('username')->unique();
 			$table->string('email')->unique();
+            $table->boolean('is_email_verified')->default(0);
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
