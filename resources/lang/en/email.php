1640aadd934ea1e23c10ac1dbcee649d0f31b14f
temito93 <temotemo161@gmail.com>
2022-11-24 01:43:58 +0400

    feat: add language

---
+++ resources/lang/en/email.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'confirmed.account' => 'Your account is confirmed, you can sign in',
+    'already.verified' => 'Your account is already verified!',
+    'sign.in' => 'SIGN IN',
+    'sent.password.reset' => 'We have sent you a password reset link',
+    'confirmation.email' => 'We have sent you a confirmation email',
+    'title.email' => 'Confirmation email',
+    'verify.email' => 'click this button to verify your email',
+    'verify.email.button' => 'VERIFY EMAIL',
+    'title.password' => 'Recover password',
+    'verify.password' => 'click this button to recover a password',
+    'verify.password.button' => 'RECOVER PASSWORD'
+];
