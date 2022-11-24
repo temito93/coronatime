85abfba88ad976f1dde5e86cffab4b737c1595ad
temito93 <temotemo161@gmail.com>
2022-11-24 17:14:38 +0400

    chore: join new username table in users table

--- database/migrations/2022_11_23_215754_users_verify.php
+++ database/migrations/2022_11_23_215754_users_verify.php
@@ -8,6 +8,7 @@ return new class extends Migration {
 	public function up()
 	{
 		Schema::create('users_verify', function (Blueprint $table) {
+			$table->id();
 			$table->integer('user_id');
 			$table->string('token');
 			$table->timestamps();
