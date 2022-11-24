c023558c1f5782bbea7824bb54352f753753cfde
temito93 <temotemo161@gmail.com>
2022-11-24 17:16:04 +0400

    chore: remove is_email_verified from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,7 +10,6 @@ return new class extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
 			$table->string('email')->unique();
-			$table->boolean('is_email_verified')->default(0);
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
