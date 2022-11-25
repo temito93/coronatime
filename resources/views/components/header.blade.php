eeeb4fbd7fa9b9c013efe6fd4a8d89077db946a5
temito93 <temotemo161@gmail.com>
2022-11-25 21:34:37 +0400

    chore: display authenticated username in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,7 +9,7 @@
         <div class="flex items-center h-8">
             <x-dropdown/>
             <div class=" flex ml-12">
-                <h2 class="text-custom-black font-bold text-base">Takeshi K.</h2>
+                <h2 class="text-custom-black font-bold text-base">{{ucwords(auth()->user()->username)}}</h2>
             </div>
             <div class="border-l h-8 ml-4 flex items-center  border-custom-neutral-200 ">
                 <x-form.button class="ml-4 text-custom-black font-medium text-sm">Log Out</x-form.button>
