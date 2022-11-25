49f1a093497a597f7d224eeca834822dfcf99521
temito93 <temotemo161@gmail.com>
2022-11-25 17:01:07 +0400

    style: dashboard main page

---
+++ resources/views/components/new-cases.blade.php
@@ -0,0 +1,9 @@
+<div class="relative">
+    <div class="bg-custom-blue-700 rounded-2xl w-[392px] h-[255px] opacity-[0.08]">
+    </div>
+    <div class="absolute top-0 w-[392px] h-[255px] flex flex-col items-center">
+        <img src="{{asset('assets/images/blue.png')}}"  width="90" alt="" class="mt-10">
+        <p class="font-medium text-xl text-custom-black mt-6">New cases</p>
+        <p class="text-custom-blue-700 font-black text-[39px] mt-4">715,523</p>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,16 @@ module.exports = {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
         extend: {
-            backgroundColor: {
-                "custom-green-500": "#0FBA68",
-            },
             colors: {
+                "custom-green-500": "#0FBA68",
                 "custom-black": "#010414",
                 "custom-blue-700": "#2029F3",
                 "custom-zinc": "#808189",
-            },
-            borderColor: {
                 "custom-neutral-200": "#E6E6E7",
                 "custom-green": "#249E2C",
                 "custom-red": "#CC1E1E",
-                "custom-neutral-100": "#F6F6F7"
-            },
-            placeholderColor: {
-                "custom-zinc": "#808189",
+                "custom-neutral-100": "#F6F6F7",
+                "custom-yellow-400": "#EAD621",
             },
         },
     },
