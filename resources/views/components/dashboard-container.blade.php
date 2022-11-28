e2c58d36dfe1d36edba5e6ad8fcfdb52d788d7bb
temito93 <temotemo161@gmail.com>
2022-11-28 21:30:41 +0400

    style: add border bottom on active link

--- resources/views/components/dashboard-container.blade.php
+++ resources/views/components/dashboard-container.blade.php
@@ -1,11 +1,15 @@
+@php
+    $dashboard = app()->getLocale()."/dashboard";
+    $byCountry = app()->getLocale()."/by_country";
+@endphp
 <x-layout>
     <x-header />
     <main class="px-[108px]">
         <div class="mt-10">
             <h2 class="text-custom-black font-extrabold text-[25px]">{{__('admin.title.worldwide')}}</h2>
             <div class="mt-10 border-b pb-4 border-solid border-custom-neutral-100">
-                <a href="/{{app()->getLocale()}}/dashboard" class="text-base font-bold text-custom-black mr-[72px] border-b-[3px] border-custom-black pb-4">{{__('admin.worldwide')}}</a>
-                <a href="/{{app()->getLocale()}}/by_country" class="pb-4 text-base font-normal text-custom-black">{{__('admin.by.country')}}</a>
+                <a href="/{{app()->getLocale()}}/dashboard" class="text-base text-custom-black mr-[72px] {{request()->is($dashboard) ? 'border-b-[3px] border-custom-black font-bold' : 'border-none font-normal' }}   pb-4">{{__('admin.worldwide')}}</a>
+                <a href="/{{app()->getLocale()}}/by_country" class="pb-4 text-base text-custom-black {{request()->is($byCountry) ? 'border-b-[3px] border-custom-black font-bold' : 'border-none font-normal' }}">{{__('admin.by.country')}}</a>
             </div>
         </div>
         {{ $slot }}
