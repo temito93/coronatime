e8bcfbfba7cc3dfbf41b5d464fc2a71b72b067a5
temito93 <temotemo161@gmail.com>
2022-11-29 13:51:08 +0400

    fix: dashboard display statistics

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -1,9 +1,9 @@
 <x-dashboard-container title="{{__('admin.title.worldwide')}}">
     <section class="mt-10">
        <div class="mt-10 flex justify-between">
-            <x-new-cases value={{!is_null($newCases) ? $newCases : '' }} />
-            <x-recovered value={{!is_null($recovered) ? $recovered : '' }} />
-            <x-death value={{!is_null($deaths) ? $deaths : '' }}/>
+            <x-new-cases value="{{!is_null($newCases) ? $newCases : '' }}" />
+            <x-recovered value="{{!is_null($recovered) ? $recovered : ''}}"  />
+            <x-death value="{{!is_null($deaths) ? $deaths : ''}}" />
        </div>
        @if(!$statistics->count())
             <div class="flex justify-center text-3xl">
