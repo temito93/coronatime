3ecfa6e17c160affe38bafe3ad4723285e69fd8f
temito93 <temotemo161@gmail.com>
2022-12-02 17:41:08 +0400

    chore: change command run to dayly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('command:statistics')->everyMinute();
+		$schedule->command('command:statistics')->daily();
 	}
 
 	protected function commands()
