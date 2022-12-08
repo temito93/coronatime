cb78d047706bfbb7d5b0c333815972acf60805a2
temito93 <temotemo161@gmail.com>
2022-12-08 17:04:23 +0400

    style: change viewport

--- resources/views/email/forgot-password.blade.php
+++ resources/views/email/forgot-password.blade.php
@@ -1,103 +1,113 @@
-<style type="text/css">
-    u + #body a {
-        color: white;
-        text-decoration: none;
-    }
-    .body {
-        width: 100%;
-        height: 100%;
-        min-height: 100%;
-        text-align: center;
-    }
-    .content {
-        text-align: center;
-        margin-top: 56px !important;
-    }
-    a {
-        color: white;
-        text-decoration: none;
-    }
+<!DOCTYPE html>
+<html lang="en" style="height: 100%">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+        />
 
-    h2 {
-        color: #010414;
-        font-weight: 900;
-        font-size: 25px;
-    }
-    p {
-        font-weight: 400;
-        font-size: 18px;
-        color: #010414;
-        margin-top: 16px;
-    }
+        <style type="text/css">
+            u + #body a {
+                color: white;
+                text-decoration: none;
+            }
+            .body {
+                width: 100%;
+                height: 100%;
+                min-height: 100%;
+                text-align: center;
+            }
+            .content {
+                text-align: center;
+                margin-top: 56px !important;
+            }
+            a {
+                color: white;
+                text-decoration: none;
+            }
 
-    .container {
-        width: 100%;
-        text-align: center;
-        margin: 0 auto;
-    }
+            h2 {
+                color: #010414;
+                font-weight: 900;
+                font-size: 25px;
+            }
+            p {
+                font-weight: 400;
+                font-size: 18px;
+                color: #010414;
+                margin-top: 16px;
+            }
 
-    .l-button {
-        padding: 19px 0;
-        display: block;
-        margin: 0 auto;
-        margin-top: 40px;
-        background-color: #0fba68;
-        border-radius: 8px;
-        width: 392px;
-        color: white;
-        text-align: center;
-        cursor: pointer;
-        font-weight: 900;
-        font-size: 16px;
-    }
+            .container {
+                width: 100%;
+                text-align: center;
+                margin: 0 auto;
+            }
 
-    .img {
-        max-width: 520px;
-    }
+            .l-button {
+                padding: 19px 0;
+                display: block;
+                margin: 0 auto;
+                margin-top: 40px;
+                background-color: #0fba68;
+                border-radius: 8px;
+                width: 392px;
+                color: white;
+                text-align: center;
+                cursor: pointer;
+                font-weight: 900;
+                font-size: 16px;
+            }
 
-    @media (max-width: 380px) {
-        h2 {
-            font-size: 20px;
-        }
-        p {
-            font-size: 16px;
-            margin-top: 8px;
-        }
+            .img {
+                max-width: 520px;
+            }
 
-        .l-button {
-            padding: 15px 0;
-            font-size:14px;
-            max-width:343px;
-            width:100%;
-        }
+            @media (max-width: 500px) {
+                h2 {
+                    font-size: 20px;
+                }
+                p {
+                    font-size: 16px;
+                    margin-top: 8px;
+                }
 
-        .body {
-            margin-left: unset;
-            text-align: center;
-            max-width: 343px;
-            width: 100%;;
-            margin-top: 16px;
-        }
+                .l-button {
+                    padding: 15px 0;
+                    font-size: 14px;
+                    max-width: 343px;
+                    width: 100%;
+                }
 
-        .container {
-            width: 100%;
-            margin: 0 auto;
-            text-align: center;
-        }
+                .body {
+                    margin-left: unset;
+                    text-align: center;
+                    max-width: 343px;
+                    width: 100%;
+                    margin-top: 16px;
+                }
 
-        .content {
-            margin: 0 auto;
-            margin-top: 40px !important;
-        }
+                .container {
+                    width: 100%;
+                    margin: 0 auto;
+                    text-align: center;
+                }
 
-        .img {
-            width: 343px;
-            width:100%;
-        }
-    }
+                .content {
+                    margin: 0 auto;
+                    margin-top: 40px !important;
+                }
 
-
-</style>
+                .img {
+                    width: 343px;
+                    width: 100%;
+                }
+            }
+        </style>
+    </head>
     <div id="body">
         <div class="body">
             <div class="container">
